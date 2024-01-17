2723e299edfa1f9c1476e97f10ee056059d3f9be
Alen Chacko <alenchacko.off@gmail.com>
2024-01-17 21:49:05 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(morgan("dev"))
+app.use(helmet())
+
+const server = () =>{
+    app.listen(port,()=>{
+        console.log(`server running on port ${port}`)
+    })
+}
+
+server()
